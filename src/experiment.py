a93041ae395d17e5818b57a23c33df61362f5d99
ShivamMangale <l3pp1n@gmail.com>
2019-04-15 21:16:09 +0530

    flask backend

--- src/experiment.py
+++ src/experiment.py
@@ -9,5 +9,34 @@ def index():
 def introduction():
     return render_template('Introduction.html')
 
+
+@app.route('/Theory')
+def introduction():
+    return render_template('Theory.html')
+
+@app.route('/Objective')
+def introduction():
+    return render_template('Objective.html')
+
+@app.route('/Experiment')
+def introduction():
+    return render_template('Experiment.html')
+
+@app.route('/Manual')
+def introduction():
+    return render_template('Manual.html')
+
+@app.route('/Quizzes')
+def introduction():
+    return render_template('Quizzes.html')
+
+@app.route('/Procedure')
+def introduction():
+    return render_template('Procedure.html')
+
+@app.route('/Further_Reading')
+def introduction():
+    return render_template('Further_Reading.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
