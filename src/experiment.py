773b6d545ec81b30d83736aef3bd193fc4d00d51
Maxslide <manas.kabre@research.iiit.ac.in>
2019-04-16 18:59:44 +0530

    Quiz db done with shuffling of Q

--- src/experiment.py
+++ src/experiment.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
+from random import shuffle
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -8,7 +12,6 @@ def index():
 @app.route('/Introduction')
 def introduction():
     return render_template('Introduction.html')
-
 
 @app.route('/Theory')
 def Theory():
@@ -28,8 +31,13 @@ def Manual():
 
 @app.route('/Quizzes')
 def Quizzes():
-    return render_template('Quizzes.html')
-
+    db.drop_all()
+    createDB()
+    a = showDB()
+    shuffle(a)
+    return render_template('Quizzes.html', Ques = a)
+# @app.route('/QuizRes')
+# def QuizRes():
 @app.route('/Procedure')
 def Procedure():
     return render_template('Procedure.html')
@@ -37,6 +45,62 @@ def Procedure():
 @app.route('/Further_Reading')
 def Further_Reading():
     return render_template('Further_Reading.html')
-
+Question = [
+    ["Which criterion ensures that we can’t find two messages that hash to the same digest?","One-wayness","Weak-collision-resistance","Strong-collision-resistance","None","Weak-collision-resistance"],
+    ["Which criterion Ensures that it must be extremely difficult or impossible to create the message if the message digest is given.","One-wayness","Weak-collision-resistance","Strong-collision resistance","None","One-wayness"],
+    ["Consider the function h: {0,1}8 -> {0,1}4. Suppose h(x) = x xmod 5 mod 16, x in [0, 255]. The collision in h occurs for.","(1, 17)","(2, 16)","(1, 16)","(2, 17)","(1, 17)"],
+    ["The Merkle-Damgard Transform is mainly useful for","Converting any fixed-length collision resistant hash function to an arbitrary length collision resistant hash function","Converting arbitrary length hash function to a fixed length hash function","Constructing hash function from random function","None","Converting arbitrary length hash function to a fixed length hash function"],
+    ["Q5","51","52","53","54","52"],
+    ["Q6","61","62","63","64","63"],
+    ["Q7","71","72","73","74","74"],
+    ["Q8","81","82","83","84","82"]]
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key = True, auto_increment = True)
+    Ques = db.Column(db.String(500),nullable = False)
+    Opt1 = db.Column(db.String(500),nullable = False)
+    Opt2 = db.Column(db.String(500),nullable = False)
+    Opt3 = db.Column(db.String(500),nullable = False)
+    Opt4 = db.Column(db.String(500),nullable = False)
+    ans = db.Column(db.String(500),nullable = False)
+def createDB():
+    db.create_all()
+    for i in Question:
+        new_ent = Quiz(Ques = i[0], Opt1 = i[1], Opt2 = i[2], Opt3 = i[3], Opt4 = i[4], ans = i[5])
+        db.session.add(new_ent)
+        db.session.commit()
+    return 
+def showDB():
+    ent = Quiz.query.all()
+    a = []
+    for i in ent:
+        b = {
+            'Q': i.Ques,
+            'o1': i.Opt1,
+            'o2': i.Opt2,
+            'o3': i.Opt3,
+            'o4': i.Opt4,
+            'ans': i.ans
+        }
+        a.append(b)
+    return a
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
